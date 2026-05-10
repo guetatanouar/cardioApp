81133ab16cf5acc0e49a3c47774ad8e7ea1da139
anouar.guetat <anouar.guetat@azertysoft.com>
2026-05-10 11:01:07 -0400

    remove unused files

--- server/fix_passwords_v2.sql
+++
@@ -1,3 +0,0 @@
--- Fix passwords properly using dollar-quoting to avoid escape issues
-UPDATE users SET password = '$2a$10$DdMVbfbLm3QqyIOIvmjjgOHXzfUcI144fPS7V7JR7WUnGdFVplkq' WHERE username = 'admin';
-UPDATE users SET password = '$2a$10$DdMVbfbLm3QqyIOIvmjjgOHXzfUcI144fPS7V7JR7WUnGdFVplkq' WHERE username = 'secretaire';
--- server/reset_password.sql
+++
@@ -1,5 +0,0 @@
--- Reset passwords
--- admin123
-UPDATE users SET password = '$2a$10$DdMVbfbLm3QqyIOIvmjjgOHXzfUcI144fPS7V7JR7WUnGdFVplkqC' WHERE username = 'admin';
--- sec123
-UPDATE users SET password = '$2a$10$DdMVbfbLm3QqyIOIvmjjgOHXzfUcI144fPS7V7JR7WUnGdFVplkqC' WHERE username = 'secretaire';
